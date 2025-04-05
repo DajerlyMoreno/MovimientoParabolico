10f4ae99c799d4c0530156a951d24045be0a2610
Juan Beltran <beltranrodriguezjuan9@gmail.com>
2025-04-05 18:38:03 -0500

    Simulación del movimiento

--- Movimiento Parabolico/Form1.Designer.cs
+++ Movimiento Parabolico/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textv0 = new System.Windows.Forms.TextBox();
             this.btnSimular = new System.Windows.Forms.Button();
             this.texttheta = new System.Windows.Forms.TextBox();
@@ -56,8 +56,12 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.masa = new System.Windows.Forms.PictureBox();
+            this.areaSimulacion = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.areaSimulacion)).BeginInit();
             this.SuspendLayout();
             // 
             // textv0
@@ -179,18 +183,15 @@
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.Location = new System.Drawing.Point(416, 38);
             this.chart1.Margin = new System.Windows.Forms.Padding(2);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series3.Name = "Trayectoria";
-            series4.ChartArea = "ChartArea1";
-            series4.Name = "Series2";
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Trayectoria";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(370, 225);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "chart1";
@@ -309,11 +310,33 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // masa
+            // 
+            this.masa.BackColor = System.Drawing.Color.Transparent;
+            this.masa.Image = ((System.Drawing.Image)(resources.GetObject("masa.Image")));
+            this.masa.Location = new System.Drawing.Point(12, 557);
+            this.masa.Name = "masa";
+            this.masa.Size = new System.Drawing.Size(44, 45);
+            this.masa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.masa.TabIndex = 19;
+            this.masa.TabStop = false;
+            // 
+            // areaSimulacion
+            // 
+            this.areaSimulacion.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.areaSimulacion.Location = new System.Drawing.Point(12, 357);
+            this.areaSimulacion.Name = "areaSimulacion";
+            this.areaSimulacion.Size = new System.Drawing.Size(1115, 245);
+            this.areaSimulacion.TabIndex = 20;
+            this.areaSimulacion.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1139, 605);
+            this.Controls.Add(this.masa);
+            this.Controls.Add(this.areaSimulacion);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -337,6 +360,8 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.areaSimulacion)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -368,6 +393,8 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox masa;
+        private System.Windows.Forms.PictureBox areaSimulacion;
     }
 }
 
--- Movimiento Parabolico/Form1.cs
+++ Movimiento Parabolico/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Movimiento_Parabolico
@@ -18,29 +19,38 @@ namespace Movimiento_Parabolico
         double y0;
         double t = 0;
 
+        //Variables para la animación
+        int altura;
+        double escala = 10;
+        int offsetY = 20;
+
         private void timer1_Tick(object sender, EventArgs e)
         {
+            x = x0 + vx * t;
+            y = y0 + vy * t - 0.5 * g * t * t;
+            double vy_t = vy - g * t;
 
-            do
+            if (y < 0)
             {
-                x = x0 + vx * t;
-                y = y0 + vy * t - 0.5 * g * t * t;
-                double vy_t = vy - g * t;
-
-                if (y < 0)
-                {
-                    timer1.Stop();
-                    break;
-                }
-
-                dataGridView1.Rows.Add(t.ToString("0.00"), x.ToString("0.00"), y.ToString("0.00"), vx.ToString("0.00"), vy_t.ToString("0.00"));
-                chart1.Series[0].Points.AddXY(x, y);
-                chart1.Series[1].Points.AddXY(x, y);
-
-                t += 0.1;
+                timer1.Stop();
+                return;
             }
-            while (true);
 
+            dataGridView1.Rows.Add(t.ToString("0.00"), x.ToString("0.00"), y.ToString("0.00"), vx.ToString("0.00"), vy_t.ToString("0.00"));
+
+            chart1.Series[0].Points.AddXY(x, y);
+
+
+            //Animacion del movimiento
+            int xPixel = Convert.ToInt32(x * 18);
+            int yPixel = Math.Min(altura - Convert.ToInt32(y * escala) - offsetY, areaSimulacion.Height - masa.Height);
+
+            if (xPixel >= 0 && xPixel <= areaSimulacion.Width - masa.Width && yPixel >= 0 && yPixel <= areaSimulacion.Height - masa.Height)
+            {
+                masa.Location = new Point(xPixel, yPixel);
+            }
+
+            t += 0.1;
         }
 
         private void btnSimular_Click(object sender, EventArgs e)
@@ -56,11 +66,18 @@ namespace Movimiento_Parabolico
             x0 = x;
             y0 = y;
 
+            t = 0;
+
+            altura = areaSimulacion.Height;
+
             dataGridView1.Rows.Clear();
 
             dataGridView1.Rows.Clear();
             chart1.Series[0].Points.Clear();
-            chart1.Series[1].Points.Clear();
+
+            areaSimulacion.Controls.Add(masa);
+
+            masa.Location = new Point(Convert.ToInt32(x0 * escala), Math.Min(altura - Convert.ToInt32(y0 * escala) - offsetY, areaSimulacion.Height - masa.Height));
 
             timer1.Enabled = true;
         }
