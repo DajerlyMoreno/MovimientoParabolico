6078316a2ac6d3c0f3ed250325ccffc46be0a497
Jazmin <dajerlymoga@gmail.com>
2025-04-04 22:16:03 -0500

    diseño

--- Movimiento Parabolico/Form1.Designer.cs
+++ Movimiento Parabolico/Form1.Designer.cs
@@ -28,16 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.textv0 = new System.Windows.Forms.TextBox();
             this.btnSimular = new System.Windows.Forms.Button();
             this.texttheta = new System.Windows.Forms.TextBox();
             this.textx0 = new System.Windows.Forms.TextBox();
             this.texty0 = new System.Windows.Forms.TextBox();
             this.textg = new System.Windows.Forms.TextBox();
-            this.textdt = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -46,15 +45,25 @@
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // textv0
             // 
-            this.textv0.Location = new System.Drawing.Point(195, 47);
+            this.textv0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textv0.Location = new System.Drawing.Point(366, 53);
             this.textv0.Name = "textv0";
-            this.textv0.Size = new System.Drawing.Size(100, 22);
+            this.textv0.Size = new System.Drawing.Size(100, 30);
             this.textv0.TabIndex = 0;
             // 
             // btnSimular
@@ -62,7 +71,7 @@
             this.btnSimular.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.btnSimular.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSimular.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnSimular.Location = new System.Drawing.Point(182, 613);
+            this.btnSimular.Location = new System.Drawing.Point(201, 362);
             this.btnSimular.Name = "btnSimular";
             this.btnSimular.Size = new System.Drawing.Size(162, 72);
             this.btnSimular.TabIndex = 1;
@@ -73,38 +82,35 @@
             // 
             // texttheta
             // 
-            this.texttheta.Location = new System.Drawing.Point(195, 118);
+            this.texttheta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.texttheta.Location = new System.Drawing.Point(366, 110);
             this.texttheta.Name = "texttheta";
-            this.texttheta.Size = new System.Drawing.Size(100, 22);
+            this.texttheta.Size = new System.Drawing.Size(100, 30);
             this.texttheta.TabIndex = 2;
             // 
             // textx0
             // 
-            this.textx0.Location = new System.Drawing.Point(195, 178);
+            this.textx0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textx0.Location = new System.Drawing.Point(366, 176);
             this.textx0.Name = "textx0";
-            this.textx0.Size = new System.Drawing.Size(100, 22);
+            this.textx0.Size = new System.Drawing.Size(100, 30);
             this.textx0.TabIndex = 3;
             // 
             // texty0
             // 
-            this.texty0.Location = new System.Drawing.Point(195, 252);
+            this.texty0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.texty0.Location = new System.Drawing.Point(366, 242);
             this.texty0.Name = "texty0";
-            this.texty0.Size = new System.Drawing.Size(100, 22);
+            this.texty0.Size = new System.Drawing.Size(100, 30);
             this.texty0.TabIndex = 4;
             // 
             // textg
             // 
-            this.textg.Location = new System.Drawing.Point(195, 319);
+            this.textg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textg.Location = new System.Drawing.Point(366, 302);
             this.textg.Name = "textg";
-            this.textg.Size = new System.Drawing.Size(100, 22);
+            this.textg.Size = new System.Drawing.Size(100, 30);
             this.textg.TabIndex = 5;
-            // 
-            // textdt
-            // 
-            this.textdt.Location = new System.Drawing.Point(195, 373);
-            this.textdt.Name = "textdt";
-            this.textdt.Size = new System.Drawing.Size(100, 22);
-            this.textdt.TabIndex = 6;
             // 
             // dataGridView1
             // 
@@ -115,11 +121,11 @@
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dataGridView1.Location = new System.Drawing.Point(528, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(1104, 47);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(756, 272);
+            this.dataGridView1.Size = new System.Drawing.Size(369, 272);
             this.dataGridView1.TabIndex = 7;
             // 
             // Column1
@@ -127,48 +133,49 @@
             this.Column1.HeaderText = "Tiempo (t)";
             this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
-            this.Column1.Width = 125;
+            this.Column1.Width = 50;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Posicion X";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
-            this.Column2.Width = 125;
+            this.Column2.Width = 50;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "Posicion Y";
             this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
-            this.Column3.Width = 125;
+            this.Column3.Width = 50;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "Velocidad X";
             this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
-            this.Column4.Width = 125;
+            this.Column4.Width = 50;
             // 
             // Column5
             // 
             this.Column5.HeaderText = "Velocidad Y";
             this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
-            this.Column5.Width = 125;
+            this.Column5.Width = 50;
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(689, 319);
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Location = new System.Drawing.Point(554, 47);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Name = "Trayectoria";
+            series8.ChartArea = "ChartArea1";
+            series8.Name = "Series2";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
             this.chart1.Size = new System.Drawing.Size(493, 277);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "chart1";
@@ -176,21 +183,120 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(422, 350);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(261, 29);
             this.label1.TabIndex = 9;
-            this.label1.Text = "label1";
+            this.label1.Text = "Velocidad Inicial (V0)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Angulo (θ)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(266, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Posicion Inicial X (x0)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(72, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 29);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Posicion Inicial Y (y0)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(76, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 29);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Gravedad (g)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(472, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "m/s";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(472, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 18);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "m";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(472, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 18);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "m";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(472, 310);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 18);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "m/s^2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(472, 118);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(15, 18);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "°";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1519, 745);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textdt);
             this.Controls.Add(this.textg);
             this.Controls.Add(this.texty0);
             this.Controls.Add(this.textx0);
@@ -214,15 +320,23 @@
         private System.Windows.Forms.TextBox textx0;
         private System.Windows.Forms.TextBox texty0;
         private System.Windows.Forms.TextBox textg;
-        private System.Windows.Forms.TextBox textdt;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- Movimiento Parabolico/Form1.cs
+++ Movimiento Parabolico/Form1.cs
@@ -12,48 +12,45 @@ namespace Movimiento_Parabolico
 
         private void btnSimular_Click(object sender, EventArgs e)
         {
-            // Obtener valores desde los cuadros de texto
             double v0 = double.Parse(textv0.Text);
-            double theta = double.Parse(texttheta.Text) * Math.PI / 180; // Convertir a radianes
+            double theta = double.Parse(texttheta.Text) * Math.PI / 180; 
             double x = double.Parse(textx0.Text);
             double y = double.Parse(texty0.Text);
-            double g = double.Parse(textg.Text);
-            double dt = 1;
-
-            // Inicializar variables
+            double g = double.Parse(textg.Text.Replace('.',','));
+            
             double t = 0;
             double vx = v0 * Math.Cos(theta);
             double vy = v0 * Math.Sin(theta);
             double x0 = x;
             double y0 = y;
 
-            // Limpiar tabla y gráfica
             dataGridView1.Rows.Clear();
             chart1.Series.Clear();
-            chart1.Series.Add("Trayectoria");
-            chart1.Series["Trayectoria"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
 
-            // Bucle de simulación
             dataGridView1.Rows.Clear();
-            chart1.Series["Trayectoria"].Points.Clear();
+            //chart1.Series["Trayectoria"].Points.Clear();
+            //chart1.Series[1].Points.Clear();
+
             do
             {
-                
-
                 x = x0 + vx * t;
                 y = y0 + vy * t - 0.5 * g * t * t;
                 double vy_t = vy - g * t;
 
-                if (y < 0) break; // cuando toca el suelo, terminar
+                if (y < 0)
+                {
 
-                dataGridView1.Rows.Add(t, x, y, vx, vy_t);
-                chart1.Series["Trayectoria"].Points.AddXY(x, y);
+                    break;
+                }
 
-                t += 1;
+                dataGridView1.Rows.Add(t.ToString("0.00"), x.ToString("0.00"), y.ToString("0.00"), vx.ToString("0.00"), vy_t.ToString("0.00"));
+                //chart1.Series["Trayectoria"].Points.AddXY(x, y);
+                //chart1.Series["Series2"].Points.AddXY(x, y);
+
+                t += 0.1;
             }
             while (true);
         }
-        // Permitir actualización de la UI
     }
 }
 
