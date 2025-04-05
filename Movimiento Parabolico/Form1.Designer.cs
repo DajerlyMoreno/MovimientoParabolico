4b3f0755fa4fc76385d5ff0bcf7bb6522d633a73
Jazmin <dajerlymoga@gmail.com>
2025-04-04 20:35:59 -0500

    creacion proyecto

---
+++ Movimiento Parabolico/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Movimiento_Parabolico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textv0 = new System.Windows.Forms.TextBox();
+            this.btnSimular = new System.Windows.Forms.Button();
+            this.texttheta = new System.Windows.Forms.TextBox();
+            this.textx0 = new System.Windows.Forms.TextBox();
+            this.texty0 = new System.Windows.Forms.TextBox();
+            this.textg = new System.Windows.Forms.TextBox();
+            this.textdt = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textv0
+            // 
+            this.textv0.Location = new System.Drawing.Point(195, 47);
+            this.textv0.Name = "textv0";
+            this.textv0.Size = new System.Drawing.Size(100, 22);
+            this.textv0.TabIndex = 0;
+            // 
+            // btnSimular
+            // 
+            this.btnSimular.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSimular.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimular.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSimular.Location = new System.Drawing.Point(182, 613);
+            this.btnSimular.Name = "btnSimular";
+            this.btnSimular.Size = new System.Drawing.Size(162, 72);
+            this.btnSimular.TabIndex = 1;
+            this.btnSimular.Text = "Simular";
+            this.btnSimular.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSimular.UseVisualStyleBackColor = false;
+            this.btnSimular.Click += new System.EventHandler(this.btnSimular_Click);
+            // 
+            // texttheta
+            // 
+            this.texttheta.Location = new System.Drawing.Point(195, 118);
+            this.texttheta.Name = "texttheta";
+            this.texttheta.Size = new System.Drawing.Size(100, 22);
+            this.texttheta.TabIndex = 2;
+            // 
+            // textx0
+            // 
+            this.textx0.Location = new System.Drawing.Point(195, 178);
+            this.textx0.Name = "textx0";
+            this.textx0.Size = new System.Drawing.Size(100, 22);
+            this.textx0.TabIndex = 3;
+            // 
+            // texty0
+            // 
+            this.texty0.Location = new System.Drawing.Point(195, 252);
+            this.texty0.Name = "texty0";
+            this.texty0.Size = new System.Drawing.Size(100, 22);
+            this.texty0.TabIndex = 4;
+            // 
+            // textg
+            // 
+            this.textg.Location = new System.Drawing.Point(195, 319);
+            this.textg.Name = "textg";
+            this.textg.Size = new System.Drawing.Size(100, 22);
+            this.textg.TabIndex = 5;
+            // 
+            // textdt
+            // 
+            this.textdt.Location = new System.Drawing.Point(195, 373);
+            this.textdt.Name = "textdt";
+            this.textdt.Size = new System.Drawing.Size(100, 22);
+            this.textdt.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(528, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(756, 272);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Tiempo (t)";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Posicion X";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Posicion Y";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Velocidad X";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Velocidad Y";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(689, 319);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(493, 277);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(422, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1519, 745);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textdt);
+            this.Controls.Add(this.textg);
+            this.Controls.Add(this.texty0);
+            this.Controls.Add(this.textx0);
+            this.Controls.Add(this.texttheta);
+            this.Controls.Add(this.btnSimular);
+            this.Controls.Add(this.textv0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textv0;
+        private System.Windows.Forms.Button btnSimular;
+        private System.Windows.Forms.TextBox texttheta;
+        private System.Windows.Forms.TextBox textx0;
+        private System.Windows.Forms.TextBox texty0;
+        private System.Windows.Forms.TextBox textg;
+        private System.Windows.Forms.TextBox textdt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Movimiento Parabolico/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Movimiento_Parabolico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimular_Click(object sender, EventArgs e)
+        {
+            // Obtener valores desde los cuadros de texto
+            double v0 = double.Parse(textv0.Text);
+            double theta = double.Parse(texttheta.Text) * Math.PI / 180; // Convertir a radianes
+            double x = double.Parse(textx0.Text);
+            double y = double.Parse(texty0.Text);
+            double g = double.Parse(textg.Text);
+            double dt = 1;
+
+            // Inicializar variables
+            double t = 0;
+            double vx = v0 * Math.Cos(theta);
+            double vy = v0 * Math.Sin(theta);
+            double x0 = x;
+            double y0 = y;
+
+            // Limpiar tabla y gráfica
+            dataGridView1.Rows.Clear();
+            chart1.Series.Clear();
+            chart1.Series.Add("Trayectoria");
+            chart1.Series["Trayectoria"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            // Bucle de simulación
+            dataGridView1.Rows.Clear();
+            chart1.Series["Trayectoria"].Points.Clear();
+            do
+            {
+                
+
+                x = x0 + vx * t;
+                y = y0 + vy * t - 0.5 * g * t * t;
+                double vy_t = vy - g * t;
+
+                if (y < 0) break; // cuando toca el suelo, terminar
+
+                dataGridView1.Rows.Add(t, x, y, vx, vy_t);
+                chart1.Series["Trayectoria"].Points.AddXY(x, y);
+
+                t += 1;
+            }
+            while (true);
+        }
+        // Permitir actualización de la UI
+    }
+}
+
